383a0bb72a00ca6fdf45b08a055f01287406e29d
gmbPop-desktop <guidobursztyn@hotmail.com>
2023-01-05 16:44:24 -0300

    done assoc

--- backend/db/associations.js
+++ backend/db/associations.js
@@ -1,22 +1,30 @@
 //Import models for bulks
+const Status = require("./models/Status");
 const User = require("./models/User");
 const Store = require("./models/Store");
 const Pet = require("./models/Pet");
-const Breed = require("./models/Breed");
+const PetBreed = require("./models/PetBreed");
+const PetType = require("./models/PetType");
 const Appointment = require("./models/Appointment");
-const Status = require("./models/Status");
 
-//Pets has one Breed, Breed could belong to many pets.
-//add breedId to the pet table
-Breed.hasOne(Pet);
-Pet.belongsTo(Breed);
-
-//Add petid to user (owner)
-User.hasOne(Pet);
+//assoc with pet/user
+User.hasMany(Pet);
 Pet.belongsTo(User);
 
-//Add storeId,userId,PetId,Status to Appointments table
-Status.hasOne(Appointment);
-Store.hasOne(Appointment);
-User.hasOne(Appointment);
-Pet.hasOne(Appointment);
+//Assoc with pet/type/breed
+PetType.hasMany(Pet);
+Pet.belongsTo(PetType);
+PetBreed.hasMany(Pet);
+Pet.belongsTo(PetBreed);
+
+//Assoc with appointment table:
+User.hasMany(Appointment);
+Appointment.belongsTo(User);
+Appointment.belongsTo(Store);
+Store.hasMany(Appointment);
+
+Status.hasMany(Appointment);
+Appointment.belongsTo(Status);
+
+Pet.hasMany(Appointment);
+Appointment.belongsTo(Pet);
