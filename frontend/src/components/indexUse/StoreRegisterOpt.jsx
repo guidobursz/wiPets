742a5ed9ff49fbf6ee2423fe0b8ceb246f084d11
gmbPop-desktop <guidobursztyn@hotmail.com>
2023-02-01 14:59:15 -0300

    done v1 register user

--- frontend/src/components/indexUse/StoreRegisterOpt.jsx
+++ frontend/src/components/indexUse/StoreRegisterOpt.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import { Paper } from "@mui/material";
+
 
 const StoreRegisterOpt = () => {
 	return (
-		<div className="mt-2 mb-2 rounded border border-warning">
-			<div className="mt-2 mb-2 d-flex justify-content-center">
-				¿Tenes una tienda? ¿Te gustaria verla en la web?
-			</div>
-			<div className="mt-1 mb-2 d-flex justify-content-center text-center">
-				<Link to={"/register/store"}>
-					<Button variant="warning">Registra tu tienda ahora!</Button>
-				</Link>
-			</div>
-		</div>
+<>
+      <Paper elevation={10}>
+      <div className="mt-2 mb-2 rounded border border-warning">
+        <div className="mt-2 mb-2 d-flex justify-content-center">
+          ¿Tenes una tienda? ¿Te gustaria verla en la web?
+        </div>
+        <div className="mt-1 mb-2 d-flex justify-content-center text-center">
+          <Link to={"/register/store"}>
+            <Button variant="warning">Registra tu tienda ahora!</Button>
+          </Link>
+        </div>
+      </div>
+
+      </Paper>
+</>
 	);
 };
 
--- frontend/src/pages/authPage/RegisterUserPage.jsx
+++ frontend/src/pages/authPage/RegisterUserPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 //Bootstrap
 import Container from "react-bootstrap/Container";
-
 //Import Components
 import Navbar from "../../components/Navbar";
 import RegisterUserForm from "../../components/authPage/register/RegisterUserForm";
@@ -10,16 +9,16 @@ import StoreRegisterOpt from "../../components/indexUse/StoreRegisterOpt";
 import Footer from "../../components/Footer";
 
 const RegisterUserPage = () => {
-	return (
-		<div>
-			<Navbar />
-			<Container>
-				<RegisterUserForm />
-				<StoreRegisterOpt />
-			</Container>
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+      <Container>
+        <RegisterUserForm />
+        <StoreRegisterOpt />
+      </Container>
+      <Footer />
+    </div>
+  );
 };
 
 export default RegisterUserPage;
